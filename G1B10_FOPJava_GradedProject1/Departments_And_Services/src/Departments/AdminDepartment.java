adb3f79c3f774355151819a4ed13fd5fef102bcf
Mariyam_Sardar <87272687+Mariyam97@users.noreply.github.com>
2022-10-07 19:47:24 +0530

    Update AdminDepartment.java

--- G1B10_FOPJava_GradedProject1/Departments_And_Services/src/Departments/AdminDepartment.java
+++ G1B10_FOPJava_GradedProject1/Departments_And_Services/src/Departments/AdminDepartment.java
@@ -24,7 +24,7 @@ public class AdminDepartment extends SuperDepartment{
 	}
 
 	public void display() {
-		System.out.println("Welcome to " + departmentName());
+		System.out.println(" Welcome to " + departmentName());
 		System.out.println(getTodaysWork());
 		System.out.println(getWorkDeadline());
 		System.out.println(isTodayAHoliday());
