e2eb70bd8c1790b4cf8bda6c19d6fd989bf5dd57
Mariyam_Sardar <87272687+Mariyam97@users.noreply.github.com>
2022-11-11 13:34:27 +0530

    Add files via upload

---
+++ FSD_G1B9_DSA/Question-1/src/com/Model/FloorConstruction.java
@@ -0,0 +1,53 @@
+package com.Model;
+
+import java.util.Stack;
+
+public class FloorConstruction {
+
+    private Stack<Integer> floorSizesStack = new Stack<>(); 
+
+	public FloorConstruction() {
+
+		System.out.println("\nThe order of construction is as follows :");
+
+	}
+    public void constructFloor(int requiredFloorSize, int[] floorSizes)
+
+	{
+
+		for (int i = 0; i < floorSizes.length; i++) {
+
+			int day = i + 1;
+
+			System.out.println("\n\nDay: " + day);
+
+			if (floorSizes[i] != requiredFloorSize) {
+
+				System.out.print(" ");
+
+				floorSizesStack.push(floorSizes[i]);
+
+			}
+            else {
+
+				requiredFloorSize -= 1;
+
+				System.out.print(floorSizes[i] + " ");
+
+				while (!floorSizesStack.isEmpty() && requiredFloorSize == floorSizesStack.peek()) 
+			
+				{
+
+					requiredFloorSize -= 1;
+
+					System.out.print(floorSizesStack.pop() + " "); 
+
+				}
+
+			}
+
+		}
+
+	}
+
+}
---
+++ FSD_G1B9_DSA/Question-2/src/com/Main/Main.java
@@ -0,0 +1,14 @@
+package com.Main;
+import com.Model.RightSkew;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RightSkew rightSkewAsc = new RightSkew();
+
+		rightSkewAsc.showRightTree();
+
+	}
+
+}
